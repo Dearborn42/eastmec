3d2d16e63ed06ff8ccfdcc90e5c861ce356fba9f
AndrewMurphy <amurf26@outlook.com>
2022-10-18 16:35:53 -0700

    Update scrollbar.js

--- scrollbar.js
+++ scrollbar.js
@@ -1,12 +1,11 @@
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 function filterFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     div = document.getElementById("myDropdown");
     a = div.getElementsByTagName("a");
+    // run through all the id's and shows the closest element
     for (i = 0; i < a.length; i++) {
         txtValue = a[i].textContent || a[i].innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
